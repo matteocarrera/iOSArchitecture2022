62a4dfee6a010dff6c88f791c86958a6d5580da2
Nikita Semenov <castlelecs@gmail.com>
2023-01-16 15:10:58 +0300

    feat: added usage example of TIDeeplink

---
+++ Architecture2022/Deeplink/AppDeeplink.swift
@@ -0,0 +1,6 @@
+import TIDeeplink
+
+enum AppDeeplink: String, DeeplinkType {
+    case profileDetails
+    case editProfile
+}
---
+++ Architecture2022/Deeplink/URL+Deeplink.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension URL {
+    static var profileDetails: URL {
+        URL(string: "app://profileDetails")!
+    }
+
+    static var editProfile: URL {
+        URL(string: "app://editProfile")!
+    }
+}
